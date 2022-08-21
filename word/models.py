e7ac636d7bf9b418ff63ec4a4e4728c149c07d95
Ali Emre Şafak <aliemresafak@gmail.com>
2022-08-22 00:15:15 +0300

    add type field

--- word/models.py
+++ word/models.py
@@ -5,8 +5,19 @@ from core.models import BaseModel
 
 
 class Word(BaseModel):
+    WordChoices = (
+        ("noun", "noun"),
+        ("pronoun", "pronoun"),
+        ("verb", "verb"),
+        ("adjective", "adjective"),
+        ("adverb", "adverb"),
+        ("preposition", "preposition"),
+        ("conjunction", "conjunction"),
+        ("interjection", "interjection"),
+    )
     name = models.CharField(max_length=100, verbose_name=_("Word"))
-    meaning = models.TextField(verbose_name=_("Word meaning"))
+    type = models.CharField(max_length=20, choices=WordChoices, null=False)
+    meaning = models.TextField(verbose_name=_("Word meaning"), null=False)
     category = models.ForeignKey(
         "category.Category", related_name="words", on_delete=models.CASCADE
     )
