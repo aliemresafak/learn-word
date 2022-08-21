bbc31ebf9e5503c9c632eed65c4768c855443492
Ali Emre Şafak <aliemresafak@gmail.com>
2022-08-22 00:15:56 +0300

    fix create and read category's data

--- word/api/serializers.py
+++ word/api/serializers.py
@@ -1,12 +1,20 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from category.api.serializers import CategorySerializer
+from category.models import Category
 from word.models import Word
 
 
 class WordSerializer(ModelSerializer):
-    category = CategorySerializer()
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        write_only=True, source="category", queryset=Category.objects.all()
+    )
 
     class Meta:
         model = Word
-        fields = ("name", "meaning", "category")
+        fields = ("name", "type", "meaning", "category", "category_id")
+
+    def create(self, validated_data):
+        return super().create(validated_data)
